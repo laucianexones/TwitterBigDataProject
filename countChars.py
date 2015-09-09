bd40ac3685b0b2070ce652b7def4ce8e2ee2e98f
Kerem@Hank-W7 <keremcumhur@gmail.com>
2015-09-09 10:41:51 +0200

    IDE Eclipse; tabs to space

--- countChars.py
+++ countChars.py
@@ -7,20 +7,22 @@ dataFileName = 'data_small.txt'
 
 
 def processData():
-        '''
-        gets data processes..documention #TODO
-        '''
-        #print dataFilePath
-        f = open(dataFilePath,'r')
-        data = f.readlines()
-
-        wordDictionary = {}
-
-        for line in data[0:5]:
-                if len(line) == 0:
-                        continue
-                print line
-                print '%d characters in tweet' % len(line)
+  '''
+  gets data processes..documention #TODO
+  '''
+  #print dataFilePath
+  f = open(dataFilePath,'r')
+  data = f.readlines()
+  
+  for line in data[0:5]:
+    if len(line) == 0:
+      continue
+    
+    #print line
+    print line
+    print repr(line)
+    print '%d characters in tweet' % len(line)
+    print '-'*10
  
 
 
@@ -31,29 +33,29 @@ dataFolder = os.path.join(os.getcwd(), "data")
 # if not, sys.exit(1)
 
 if os.path.exists(dataFolder):
-        dataFilePath = os.path.join(dataFolder, dataFileName)
-        if os.path.isfile(dataFilePath):
-                processData()
+  dataFilePath = os.path.join(dataFolder, dataFileName)
+  if os.path.isfile(dataFilePath):
+    processData()
 
-        else:
-                print '%s not found' % dataFileName
-                sys.exit(1)
+  else:
+    print '%s not found' % dataFileName
+    sys.exit(1)
 else:
-        print '%s not found' % dataFolder
-        sys.exit(1)
+  print '%s not found' % dataFolder
+  sys.exit(1)
 
 
 
 
 # commented out, will use later			
-#sorted_dict = sorted(wordDictionary.items(), key=operator.itemgetter(0))
-#print sorted_dict
-##               for word in line.split():
-##                        
-##                        if not wordDictionary.has_key(word):
-##                                wordDictionary[word] = 1
-##                        else:
-##                                wordDictionary[word] += 1
-##
-##                print wordDictionary
+# wordDictionary = {}
+# sorted_dict = sorted(wordDictionary.items(), key=operator.itemgetter(0))
+# print sorted_dict
+#               for word in line.split():
+#                        
+#                        if not wordDictionary.has_key(word):
+#                               wordDictionary[word] = 1
+#                        else:##                                wordDictionary[word] += 1
+#
+#                print wordDictionary
 
